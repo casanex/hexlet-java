f06761c18a64e82e0aaa8d5ac98f195e5b184c81
casanex <127040026+casanex@users.noreply.github.com>
2023-03-11 20:05:13 +0300

    Update One.java

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -2,6 +2,8 @@ package io.hexlet;
 import org.apache.commons.lang3.StringUtils;
 public class One {
     public static void main(String[] args) {
-        Two.reverse();
+        
+        System.out.println(reverse("Hexlet");
+        
     }
 }
